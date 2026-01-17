fbb7aaac5f70541ce424a082b79ab5278d06a5fd
deepthi2005-aiml <deepthipaulmahendran81@gmail.com>
2026-01-17 16:27:12 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { Sidebar } from './components/Sidebar';
 import { TemplateGallery } from './components/TemplateGallery';
